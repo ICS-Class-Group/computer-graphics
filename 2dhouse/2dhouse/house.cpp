7aa4013f0f8556e9c7d56591cfcb69aa51a248b5
Bett <felicia.bett@strathmore.edu>
2021-11-03 14:49:07 +0300

    Add door

--- 2dhouse/2dhouse/house.cpp
+++ 2dhouse/2dhouse/house.cpp
@@ -1,35 +1,64 @@
 #include<iostream>
 #include<GL/glut.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include<math.h>
 
 void init() {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
-	gluOrtho2D(-15, 15, -15, 15);
+	gluOrtho2D(-20, 20, -20, 20);
 	glClear(GL_COLOR_BUFFER_BIT);
-
 }
-
-void mainhouse() {
-	glColor3f(0.0, 0.0, 1.0);
+void door() {
+	glColor3f(0.0, 1.0, 0.0);
 	glBegin(GL_LINE_LOOP);
-	glVertex2f(-11, -11);
-	glVertex2f(11, -11);
-	glVertex2f(11, 1);
-	glVertex2f(13, 1);
-	glVertex2f(13, 4);
-	glVertex2f(-13, 4);
-	glVertex2f(-13, 1);
-	glVertex2f(-11, 1);
+	glVertex2f(-2, -12);
+	glVertex2f(2, -12);
+	glVertex2f(2, -2);
+	glVertex2f(-2, -2);
 	glEnd();
 	glFlush();
 }
 
+void doorknob() {
+
+		glColor3f(0.0, 0.0, 1.0);
+		glPointSize(6.0);
+		glBegin(GL_POINTS);
+		glVertex2f(1.4, -7);
+		glEnd();
+		glFlush();
+}
+
+
+void mainhouse() {
+	glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(-12, -12);
+	glVertex2f(12, -12);
+	glVertex2f(12, 2);
+	glVertex2f(14, 2);
+	glVertex2f(14, 6);
+	glVertex2f(-14, 6);
+	glVertex2f(-14, 2);
+	glVertex2f(-12, 2);
+	glEnd();
+	glFlush();
+}
+
+void house() {
+	mainhouse();
+	door();
+	doorknob();
+}
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(50, 100);
 	glutCreateWindow("House");
 
 	init();
-	glutDisplayFunc(mainhouse);
+	glutDisplayFunc(house);
 	glutMainLoop();
 }
